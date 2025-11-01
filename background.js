087521c1c326f8fd8ffe11728d38cdead17c10ea
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-11-01 16:15:45 -0500

    added C-v and M-v

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ browser.commands.getAll().then(console.log);
 browser.commands.onCommand.addListener(async (command) => {
     const [tab] = await browser.tabs.query({active: true, currentWindow: true});
     switch (command) {
-    case "next-line":
+    case "next-line": // C-n
 	if (tab.id) {
 	    try {
 		await browser.scripting.executeScript({
@@ -15,12 +15,39 @@ browser.commands.onCommand.addListener(async (command) => {
 	    }
 	}
 	break;
-    case "previous-line":
+	
+    case "previous-line": // C-p
 	if (tab.id) {
 	    try {
 		await browser.scripting.executeScript({
 		    target: {tabId: tab.id, allFrames: false},
 		    func: () => window.scrollBy({top: -40, left: 0, behavior: "instant"})
+		});
+	    } catch (e) {
+		console.error("injection failed:", e);
+	    }
+	}
+	break;
+	
+    case "scroll-up-command": // C-v
+	if (tab.id) {
+	    try {
+		await browser.scripting.executeScript({
+		    target: {tabId: tab.id, allFrames: false},
+		    func: () => window.scrollBy({top: window.innerHeight - 40, left: 0, behavior: "instant"})
+		});
+	    } catch (e) {
+		console.error("injection failed:", e);
+	    }
+	}
+	break;
+
+    case "scroll-down-command": // M-v
+	if (tab.id) {
+	    try {
+		await browser.scripting.executeScript({
+		    target: {tabId: tab.id, allFrames: false},
+		    func: () => window.scrollBy({top: -window.innerHeight + 40, left: 0, behavior: "instant"})
 		});
 	    } catch (e) {
 		console.error("injection failed:", e);
