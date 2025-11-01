fe2021141ca9a30f079a4c4b9be8159eabcec280
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-11-01 16:57:11 -0500

    added C-f and C-b

--- background.js
+++ background.js
@@ -53,5 +53,32 @@ browser.commands.onCommand.addListener(async (command) => {
 	}
 	break;
 	
+    case "forward-char": // C-f
+	if (tab.id) {
+	    console.log("forward-char");
+	    try {
+		await browser.scripting.executeScript({
+		    target: {tabId: tab.id, allFrames: false},
+		    func: () => window.scrollBy({top: 0, left: 40, behavior: "instant"})
+		});
+	    } catch (e) {
+		console.error("injection failed:", e);
+	    }
+	}
+	break;
+
+    case "backward-char": // C-b
+	if (tab.id) {
+	    try {
+		await browser.scripting.executeScript({
+		    target: {tabId: tab.id, allFrames: false},
+		    func: () => window.scrollBy({top: 0, left: -40, behavior: "instant"})
+		});
+	    } catch (e) {
+		console.error("injection failed:", e);
+	    }
+	}
+	break;
+	
     }
 });
